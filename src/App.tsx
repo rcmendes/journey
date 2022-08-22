311c7e3bdf82fa1acd2c86920bf136cb71a441d6
Rodrigo Mendes <rcmendes80@gmail.com>
2022-08-22 11:56:12 -0400

    Added Markdown editor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import YAML from "yaml";
 import Editor from "@monaco-editor/react";
-
+import MDEditor from "@uiw/react-md-editor";
 interface Event {
     title: string;
     tags: string[];
@@ -56,11 +56,11 @@ function App() {
     return (
         <JourneyProvider>
             <div className="flex flex-col w-screen h-screen bg-slate-100">
-                <JourneyHeader />
                 <main className="flex flex-row grow">
-                    <JourneyEditor/>
-                    <div className="w-2 cursor-move bg-slate-300 hover:bg-slate-700 hover:shadow-lg"/>
+                    <JourneyEditor />
+                    <div className="w-2 cursor-move bg-slate-300 hover:bg-slate-700 hover:shadow-lg" />
                     <div className="flex flex-col">
+                    <JourneyHeader />
                         <JourneyMap />
                         <TechnicalNotes />
                     </div>
@@ -75,9 +75,9 @@ const JourneyHeader = () => {
     const journey = handler.data;
 
     return (
-        <header className="p-2 text-center text-white bg-slate-700">
+        <header className="p-1 text-center text-white bg-slate-700">
             <h1 className="text-xl font-bold">{journey.title}</h1>
-            <p className="mt-1 text-sm font-light">{journey.description}</p>
+            <p className="text-sm font-light">{journey.description}</p>
         </header>
     );
 };
@@ -86,28 +86,55 @@ const JourneyEditor = () => {
     const handler = useJourney();
     const journey = handler.data;
 
-    function handleEditorChange(value : string |undefined, event: any) {
+    function handleEditorChange(value: string | undefined, event: any) {
         // console.log("here is the current model value:", value, "| type:", typeof(event));
-        if (typeof(value) === "string") {
+        if (typeof value === "string") {
             handler.update(value);
         }
-        
     }
 
     return (
         <div className="flex flex-col text-black border-r-4 border-slate-300 basis-3/5">
-            <div className="text-lg text-white bg-slate-500">TOOLBAR</div>
-            <Editor defaultLanguage="yaml" defaultValue={YAML.stringify(journey)} onChange={handleEditorChange} theme="vs-dark" options={{fontSize:12,minimap:{enabled:false}}} />
+            <div className="px-1 text-lg font-bold text-white bg-slate-500">
+                Journey Descriptor
+            </div>
+            <Editor
+                defaultLanguage="yaml"
+                defaultValue={YAML.stringify(journey)}
+                onChange={handleEditorChange}
+                theme="vs-dark"
+                options={{ fontSize: 12, minimap: { enabled: false } }}
+            />
         </div>
     );
 };
 
 const TechnicalNotes = () => {
+    const [value, setValue] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const text = await loadMDExample();
+            setValue(text);
+        };
+
+        fetchData().catch(console.error);
+    }, []);
+
+    function update(value: string | undefined) {
+        if (typeof value === "string") {
+            setValue(value);
+        }
+    }
+
     return (
-        <div className="p-2 mt-4 border-t-2 border-dashed text-slate-700 grow">
-            <h1 className="text-lg font-bold text-slate-700">
+        <div className="flex flex-col p-2 mt-4 border-t-2 border-dashed grow">
+            <h1 className="px-1 text-lg font-bold text-white bg-slate-500">
                 Technical notes
             </h1>
+            {/* <Editor defaultLanguage="markdown" defaultValue="# Hello!!!" theme="vs-dark" options={{fontSize:12,minimap:{enabled:false}}} /> */}
+            <MDEditor value={value} onChange={update} className="grow"/>
+            {/* <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} /> */}
         </div>
     );
 };
@@ -118,54 +145,63 @@ const JourneyMap = () => {
 
     return (
         <div className="flex gap-1 justify-evenly">
-            {journey.chapters && journey.chapters.map((chapter, idx) => {
-                return (
-                    <div
-                        key={idx}
-                        className={`w-full pl-2 pr-1 ${idx > 0 ? "border-l-2" : ""}`}
-                    >
-                        <h1 className="px-1 text-lg font-bold text-white border-b-2 rounded-md border-b-slate-300 bg-slate-500">
-                            {chapter.title}
-                        </h1>
-                        <div className="flex flex-wrap justify-center">
-                            {chapter.events && chapter.events.map((evt, idx) => {
-                                return (
-                                    <div
-                                        key={idx}
-                                        className="flex flex-col justify-between p-2 m-2 bg-white shadow-lg text-slate-900 w-60"
-                                    >
-                                        <span className="text-sm">
-                                            {evt.title}
-                                        </span>
-                                        <div className="flex justify-between gap-1 mt-1 text-xs text-center">
-                                            <div className="flex gap-1">
-                                                {evt.tags && evt.tags.map(
-                                                    (tag, idx) => {
-                                                        return (
-                                                            <span
-                                                                className="px-1 text-white bg-blue-700 rounded-md"
-                                                                key={idx}
-                                                            >
-                                                                {tag}
-                                                            </span>
-                                                        );
-                                                    }
-                                                )}
-                                            </div>
-                                            <span
-                                                className="w-4 h-4 font-light text-white bg-indigo-900 rounded-full opacity-80"
+            {journey.chapters &&
+                journey.chapters.map((chapter, idx) => {
+                    return (
+                        <div
+                            key={idx}
+                            className={`w-full pl-2 pr-1 ${
+                                idx > 0 ? "border-l-2" : ""
+                            }`}
+                        >
+                            <h1 className="px-1 text-lg font-bold text-white border-b-2 rounded-md border-b-slate-300 bg-slate-500">
+                                {chapter.title}
+                            </h1>
+                            <div className="flex flex-wrap justify-center">
+                                {chapter.events &&
+                                    chapter.events.map((evt, idx) => {
+                                        return (
+                                            <div
                                                 key={idx}
+                                                className="flex flex-col justify-between p-2 m-2 bg-white shadow-lg text-slate-900 w-60"
                                             >
-                                                {idx + 1}
-                                            </span>
-                                        </div>
-                                    </div>
-                                );
-                            })}
+                                                <span className="text-sm">
+                                                    {evt.title}
+                                                </span>
+                                                <div className="flex justify-between gap-1 mt-1 text-xs text-center">
+                                                    <div className="flex gap-1">
+                                                        {evt.tags &&
+                                                            evt.tags.map(
+                                                                (tag, idx) => {
+                                                                    return (
+                                                                        <span
+                                                                            className="px-1 text-white bg-blue-700 rounded-md"
+                                                                            key={
+                                                                                idx
+                                                                            }
+                                                                        >
+                                                                            {
+                                                                                tag
+                                                                            }
+                                                                        </span>
+                                                                    );
+                                                                }
+                                                            )}
+                                                    </div>
+                                                    <span
+                                                        className="w-4 h-4 font-light text-white bg-indigo-900 rounded-full opacity-80"
+                                                        key={idx}
+                                                    >
+                                                        {idx + 1}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
         </div>
     );
 };
@@ -275,9 +311,12 @@ chapters:
     tags: []
   - title: Chapter 3, event D
     tags: []
-`
-for (let i=0; i<3; i++) {
+`;
+for (let i = 0; i < 3; i++) {}
 
+async function loadMDExample() {
+    const response = await fetch("/examples/ex1.md");
+    return await response.text();
 }
 
 export default App;
